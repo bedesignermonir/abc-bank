216089acf4901d779aa1eb04eebbd863dc9d459f
bedesignermonir <clkmonir@gmail.com>
2023-02-25 00:17:59 +0600

    login area added

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('sign-in').addEventListener('click', function () {
+    const emailVerify = document.getElementById('email-input');
+    emailValue = emailVerify.value;
+
+    const passwordVerify = document.getElementById('password-input');
+    passwordValue = passwordVerify.value;
+
+    if (emailValue === 'abc@computer.com' && passwordValue === '123456') {
+        window.location.href = 'bank.html';
+    }
+    else {
+        alert("You are not a valid user")
+    }
+
+})
